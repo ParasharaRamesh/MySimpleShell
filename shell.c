3e8c1387595034e42ba9f2e1d71d0b117f77402d
nitishmakam <nitishmakam@gmail.com>
2018-03-29 21:09:35 +0530

    Pipe redirection

--- shell.c
+++ shell.c
@@ -65,7 +65,91 @@ void init(void)
   }
 }
 
+// function for finding pipe
+int parsePipe(char* str, char** strpiped)
+{
+    int i;
+    for (i = 0; i < 2; i++) {
+        strpiped[i] = strsep(&str, "|");
+        if (strpiped[i] == NULL)
+            break;
+    }
+ 
+    if (strpiped[1] == NULL)
+        return 0; // returns zero if no pipe is found.
+    else {
+        return 1;
+    }
+}
+ 
+void execArgsPiped(char** parsed, char** parsedpipe)
+{
+    // 0 is read end, 1 is write end
+    int pipefd[2]; 
+    pid_t p1, p2;
+ 
+    if (pipe(pipefd) < 0) {
+        printf("\nPipe could not be initialized");
+        return;
+    }
+    p1 = fork();
+    if (p1 < 0) {
+        printf("\nCould not fork");
+        return;
+    }
+ 
+    if (p1 == 0) {
+        // Child 1 executing..
+        // It only needs to write at the write end
+        close(pipefd[0]);
+        dup2(pipefd[1], STDOUT_FILENO);
+        close(pipefd[1]);
+ 
+        if (execvp(parsed[0], parsed) < 0) {
+            printf("\nCould not execute command 1..");
+            exit(0);
+        }
+    } else {
+        // Parent executing
+        p2 = fork();
+ 
+        if (p2 < 0) {
+            printf("\nCould not fork");
+            return;
+        }
+ 
+        // Child 2 executing..
+        // It only needs to read at the read end
+        if (p2 == 0) {
+            close(pipefd[1]);
+            dup2(pipefd[0], STDIN_FILENO);
+            close(pipefd[0]);
+            if (execvp(parsedpipe[0], parsedpipe) < 0) {
+                printf("\nCould not execute command 2..");
+                exit(0);
+            }
+        } else {
+            // parent executing, waiting for two children
+            wait(NULL);
+            wait(NULL);
+        }
+    }
+}
 
+void parseSpace(char* str, char** parsed)
+{
+    int i;
+ 
+    for (i = 0; i < 100; i++) {
+        parsed[i] = strsep(&str, " ");
+ 
+        if (parsed[i] == NULL)
+            break;
+        if (strlen(parsed[i]) == 0)
+            i--;
+    }
+}
+ 
 void logalias(char * original, char * alias)
 {
   strcpy(aliasHistory[aliasno].aliascommand,alias);
@@ -106,8 +190,9 @@ void printRecentCommands()
             if(temp<0)
                 temp=24;
             i++;
-            if(i==25)
-                break;
+            if(i==25){
+            	break;
+            }
         }
         else{
             break;
@@ -243,12 +328,19 @@ int main(void)
 {
   init();
   char prompt[3]="$$\0";
+  char *parsedArgs[100];
+  char* parsedArgsPiped[100];
   char input[50];
   while(1)
   {
       printf("%s ",prompt);
       gets(input);
-
+      char* strpiped[2];
+      if(parsePipe(input,strpiped)){
+      	parseSpace(strpiped[0],parsedArgs);
+      	parseSpace(strpiped[1],parsedArgsPiped);
+      	execArgsPiped(parsedArgs,parsedArgsPiped);
+      }
       if(strcmp(input,"quit")==0)
       {
           exit(0);
